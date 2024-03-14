943b105603ca6ff3ccbdc4356bed5122d1652764
Hai204td <157661199+Hai204td@users.noreply.github.com>
2024-03-14 08:11:38 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        MP3 mp3;
+        MP3 mp3 = new MP3("MP3 Player", "Portable music player", "3001", 150000, 3600);
+        Book book = new Book("Dem nay dau tinh yeu nay","Tieu thuyet","2004",80000,"Hariko",350,"Tinh yeu");
+        System.out.println("MP3 Info");
         mp3.showInfo();
-
-        Book book;
-        book.showInfo();
-
+        System.out.println("Book Info");
     }
 }
